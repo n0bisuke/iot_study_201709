316f4ab2af24d15652b6850ba545e1f5a313c677
のびすけ <rsksound@gmail.com>
2017-09-07 22:49:54 +0900

    update

---
+++ day4/sounddetect/sounddetect.ino
@@ -0,0 +1,35 @@
+#define WIO_DIGITAL 2
+#define PIN_ADC A0
+#define THRESHOLD 500 //閾値
+
+int val = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(WIO_DIGITAL, OUTPUT);
+}
+
+void loop() {
+  val = cal();
+  Serial.print(val);
+  if(val >= THRESHOLD){
+    digitalWrite(WIO_DIGITAL, HIGH);
+    Serial.println(": 反応あり");
+  }else{
+    digitalWrite(WIO_DIGITAL, LOW);
+    Serial.println(": 反応なし");
+  }
+
+  delay(100);
+}
+
+int cal(){
+    long sum = 0;
+    
+    for(int i=0; i<32; i++){
+      sum += analogRead(PIN_ADC);
+    }
+    sum >>= 5;
+
+    return sum;
+}
