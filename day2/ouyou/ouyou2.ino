061e1593ddd73e25dcbe205cdd5979d4646f7a02
のびすけ <rsksound@gmail.com>
2017-09-07 11:01:05 +0900

    update

--- day2/ouyou/ouyou2.ino
+++ day2/ouyou/ouyou2.ino
@@ -2,6 +2,7 @@
 #include <Digital_Light_TSL2561.h>
 #define LED_PIN 13
 #define THRESHOLD 100
+#define LED_DELAY_TIME 100
 #define DELAY_TIME 50
 
 void setup()
@@ -16,11 +17,11 @@ void loop()
 {
   Serial.print("The Light value is: ");
   Serial.println(TSL2561.readVisibleLux());
-  if(TSL2561.readVisibleLux() < THRESHOLD){
+  if(TSL2561.readVisibleLux() < THRESHOLD){
     digitalWrite(LED_PIN, HIGH);
-    delay(DELAY_TIME);
+    delay(LED_DELAY_TIME);
     digitalWrite(LED_PIN, LOW);
-    delay(DELAY_TIME);
+    delay(LED_DELAY_TIME);
   }else{
     digitalWrite(LED_PIN, LOW);
   }
