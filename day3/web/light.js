86c770b5272f82aeeb14af890406b7d6210cfffb
のびすけ <rsksound@gmail.com>
2017-09-07 23:27:04 +0900

    update

--- day3/web/light.js
+++ day3/web/light.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const TOKEN = `b71bde51ca76124e888f700369b1236e`;
+const TOKEN = `トークンを指定`;
 const URL = `http://cors-allow.azurewebsites.net/?url=https://us.wio.seeed.io/v1/node/GroveDigitalLightI2C0/lux?access_token=${TOKEN}`;
 const $bulb = document.querySelector('.bulb');
 const INTERVAL_TIME = 3000;
